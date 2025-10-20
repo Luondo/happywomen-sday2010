d31ae20b38bc658e2124949e933f4d5550ac9370
luondo <luondo@example.com>
2025-10-20 12:59:44 +0700

    addchange

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+
+  const text = "Chúc toàn thể nhân viên nữ (phòng tổ chức hành chính with love) có một ngày 20/10 tràn đầy niềm vui, yêu thương và hạnh phúc❤️.";
+  const greeting = document.getElementById("greeting");
+  let index = 0;
+
+  function typeEffect() {
+    if (index < text.length) {
+      greeting.textContent += text.charAt(index);
+      index++;
+      setTimeout(typeEffect, 20);
+    }
+  }
+
+  typeEffect();
+};
